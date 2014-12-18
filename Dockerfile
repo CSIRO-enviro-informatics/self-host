bb8e529ff953e65a1e67f5fafe400d8ffa1248f3
Ben Leighton <ben.leighton@csiro.au>
2014-12-18 15:52:33 +1100

    sass extras enables sass in docker default install bourbon neat and bitters

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM eboraas/apache
+RUN apt-get update
+RUN apt-get install -y ruby-full
+RUN gem install sass
+RUN gem install bourbon
+RUN gem install neat 
+RUN mkdir -p /var/www/stylesheets  
+CMD /usr/sbin/apache2ctl -D FOREGROUND && sass --watch /var/www/stylesheets:/var/www/stylesheets
--- host.sh
+++ host.sh
@@ -2,7 +2,8 @@
 for HTTPPORT in $(seq 8080 8442); do echo -ne "\035" | telnet 127.0.0.1 $HTTPPORT > /dev/null 2>&1; [ $? -eq 1 ] && break; done
 for SSLPORT in $(seq 8443 10000); do echo -ne "\035" | telnet 127.0.0.1 $SSLPORT > /dev/null 2>&1; [ $? -eq 1 ] && break; done
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-CONTAINERID=$(docker run -p $HTTPPORT:80 -p $SSLPORT:443 -v $DIR:/var/www/ -d eboraas/apache)
+docker build -t apache/sass-extra .
+CONTAINERID=$(docker run -p $HTTPPORT:80 -p $SSLPORT:443 -v $DIR:/var/www/ -d apache/sass-extra)
 echo "running at http://localhost:$HTTPPORT and https://localhost:$SSLPORT"
 NAME=$(docker inspect --format='{{.Name}}' $CONTAINERID)
 NAME=${NAME:1}
